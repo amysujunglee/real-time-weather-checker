1fe3e6a7601521d5bfa13ba8c6084dcc8031e763
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2023-01-13 20:25:57 -0800

    Update app.js

--- app.js
+++ app.js
@@ -29,28 +29,3 @@ function getWeatherData() {
     })
     .catch(error => alert("Please enter a correct country and city name!"));
 }
-
-// XMLHttpRequest version.
-// function getWeatherData() {
-//   let cityName = document.getElementById("city-name").value;
-//   let countryCode = document.getElementById("country-code").value;
-
-//   weatherConditions.open(
-//     "GET",
-//     `//api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&appid=28155f48901bf4bd88a7799d5ea9eeca&units=metric`,
-//     true
-//   );
-//   weatherConditions.responseType = "text";
-//   weatherConditions.send(null);
-
-//   weatherConditions.onload = function() {
-//     if (weatherConditions.status === 200) {
-//       weatherResult = JSON.parse(weatherConditions.responseText);
-
-//       let resultDiv = document.getElementById("resultDiv");
-//       resultDiv.innerHTML = `${weatherResult.name}<br>Temperature: ${weatherResult.main.temp}&deg;<br>Humidity: ${weatherResult.main.humidity}%<br>Description: ${weatherResult.weather[0].description}`;
-//     } else {
-//       alert("Please enter a correct city and country name!");
-//     }
-//   };
-// }
